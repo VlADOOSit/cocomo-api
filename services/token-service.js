2e2eedc2fe49161761d061e902f0aea3d3094ffe
vlad <vlad.kharkovskiy10@gmail.com>
2024-09-05 17:32:05 +0300

    add function closed #2 @4h

--- services/token-service.js
+++ services/token-service.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config.json');
 
 
+
 class TokenService {
     generateTokens(payload) {
         const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, {expiresIn: config.jwt.access_expire_at})
