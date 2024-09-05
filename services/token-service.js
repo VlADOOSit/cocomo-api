5dc2a5fa96fbc886b9c0c9fed32b6b1ac046ce58
vlad <vlad.kharkovskiy10@gmail.com>
2024-09-05 17:42:16 +0300

    add function refs #2 @4h

--- services/token-service.js
+++ services/token-service.js
@@ -4,6 +4,7 @@ const config = require('../config.json');
 const JWT_ACCESS_SECRET = config.jwt.jwt_access_secret;
 const JWT_REFRESH_SECRET = config.jwt.jwt_refresh_secret;
 
+
 class TokenService {
     generateTokens(payload) {
         const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, {expiresIn: config.jwt.access_expire_at})
