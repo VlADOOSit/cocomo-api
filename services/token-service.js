ededec3ba9fb45ea5bf52ef8d5f2c6cb874cffa4
vlad <vlad.kharkovskiy10@gmail.com>
2024-09-05 17:33:41 +0300

    add function fixes #2

--- services/token-service.js
+++ services/token-service.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config.json');
 
-
 class TokenService {
     generateTokens(payload) {
         const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, {expiresIn: config.jwt.access_expire_at})
