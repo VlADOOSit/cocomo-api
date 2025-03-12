e3b25d7268e2f1b3d6386a21432deb902ce3ce40
vlad <vlad.kharkovskiy10@gmail.com>
2025-03-12 15:43:18 +0200

    add docker

--- db/sql/initial_db.sql
+++ db/sql/initial_db.sql
@@ -1,4 +1,25 @@
-SET GLOBAL validate_password_policy = 0;
 CREATE USER IF NOT EXISTS 'vkharkivsk'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'vkharkivsk'@'localhost' WITH GRANT OPTION;
 CREATE DATABASE IF NOT EXISTS cocomo;
+
+USE cocomo;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    login VARCHAR(31) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS calc_models (
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    user_id INT(11) NOT NULL,
+    kloc FLOAT NOT NULL,
+    project_type VARCHAR(255) NOT NULL,
+    saving_type VARCHAR(255) NOT NULL,
+    rating_attr JSON,
+    advancedFlag BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
