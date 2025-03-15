f15cdbabfb1e1353ffd21f45bd1f2ec97f85dc5a
vlad <vlad.kharkovskiy10@gmail.com>
2025-03-15 15:35:12 +0200

    add nginx

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const HOST = config.server.host
 const app = express();
 
 app.use(cors({
-    origin: ['http://192.168.20.251:3000', 'http://localhost:3000'],
+    origin: ['http://websystem-docker.com', 'http://192.168.20.251:3000', 'http://localhost:3000'],
     credentials:true,
     methods:["GET" , "POST" , "PUT", "PATCH", "DELETE"],
     optionSuccessStatus:200
@@ -28,4 +28,6 @@ app.use('/api/calc', CalcModelRouter);
 
 app.use(ErrorHandler);
 
-app.listen(PORT, () => {console.log(`Server start on https://${HOST}:${PORT}`)});
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server start on http://${HOST}:${PORT}`);
+});
